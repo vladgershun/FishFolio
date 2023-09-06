01d20a272cf9d46e31e78ed8c83eb6367c948da7
Vlad Gershun <dev.vladgershun@gmail.com>
2023-09-06 13:29:19 -0700

    Added more delete options to settings.

--- FishFolio/Settings/SettingsView.swift
+++ FishFolio/Settings/SettingsView.swift
@@ -13,7 +13,10 @@ struct SettingsView: View {
     @State private var lengthUnits: LengthUnits = .inches
     @State private var temperatureUnits: TemperatureUnits = .fahrenheit
     
-    @State private var isShowing = false
+    @State private var speciesData = false
+    @State private var baitData = false
+    @State private var locationData = false
+    @State private var allData = false
     
     var body: some View {
         NavigationView {
@@ -81,11 +84,35 @@ struct SettingsView: View {
                     
                     
                     Section {
-                        Button("Delete All Data", role: .destructive) { isShowing = true }
+                        Button("Delete Species Data", role: .destructive) { speciesData = true }
+                            .foregroundColor(.orange)
+                        Button("Delete Bait Data", role: .destructive) { baitData = true }
+                            .foregroundColor(.orange)
+                        Button("Delete Location Data", role: .destructive) { locationData = true }
+                            .foregroundColor(.orange)
+                    }
+                    
+                    Section {
+                        Button("Delete All Data", role: .destructive) { allData = true }
                     }
                 }
                 .navigationTitle("Settings")
-                .confirmationDialog("Delete All Data?", isPresented: $isShowing) {
+                .confirmationDialog("Delete All Species?", isPresented: $speciesData) {
+                    Button("Delete", role: .destructive) { }
+                } message: {
+                    Text("This will delete all user generated species")
+                }
+                .confirmationDialog("Delete All Bait?", isPresented: $baitData) {
+                    Button("Delete", role: .destructive) { }
+                } message: {
+                    Text("This will delete all user generated bait")
+                }
+                .confirmationDialog("Delete All Locations?", isPresented: $locationData) {
+                    Button("Delete", role: .destructive) { }
+                } message: {
+                    Text("This will delete all user generated locations")
+                }
+                .confirmationDialog("Delete All Data?", isPresented: $allData) {
                     Button("Delete", role: .destructive) { }
                 } message: {
                     Text("You cannont undo this action")
